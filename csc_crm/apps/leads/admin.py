03bfd5d4ab708be2aefa72e381380a45fca95f4a
Sathish Vishnu <sathishvishnuarishkumar@gmail.com>
2026-04-24 11:28:33 +0530

    Completed Backend logics

--- csc_crm/apps/leads/admin.py
+++ csc_crm/apps/leads/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Lead)
-admin.site.register(CallLog)
-admin.site.register(Follow_up)
+admin.site.register(LeadCapture)
--- csc_crm/apps/leads/models.py
+++ csc_crm/apps/leads/models.py
@@ -2,87 +2,76 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Lead Model
-class Lead(models.Model):
+class LeadCapture(models.Model):
+
+    COURSE_CHOICES  = [
+        ('python', 'Python Full Stack'),
+        ('java', 'Java Full Stack'),
+        ('front_end','Front End'),
+        ('back_end', 'Back End'),
+        ('data_science', 'Data Science'),
+        ('ai', 'Artificial Intelligence')
+    ]
+
+    SOURCE_CHOICES = [
+        ('walk_in', 'Walk-in'),
+        ('referral', 'Referral'),
+        ('social_media', 'Social Media'),
+        ('advertisement', 'Advertisement'),
+        ('phone_enquiry', 'Phone Enquiry'),
+        ('website', 'Website'),
+    ]
 
     STATUS_CHOICES = [
         ('new', 'New'),
         ('contacted', 'Contacted'),
         ('interested', 'Interested'),
-        ('converted', 'Converted'),
-        ('lost', 'Lost'),
+        ('demo_scheduled', 'Deme Scheduled'),
+        ('enrolled', 'Enrolled'),
+        ('lost', 'Lost')
     ]
 
-    MODE = [
-        ('online', 'Online'),
-        ('offline', 'Offline')
-    ]
-
-    SOURCE_CHOICES = [
-        ('website', 'Website'),
-        ('call', 'Call'),
-        ('facebook', 'Facebook'),
-        ('instagram', 'Instagram'),
-        ('walkin', 'Walk-In'),
-        ('referral', 'Referral'),
-    ]
-
-    #Basic Information of Leads
-    full_name = models.CharField(max_length=100,)
+    lead_id = models.CharField(max_length=10, unique=True)
+    lead_name = models.CharField(max_length=100,)
     email = models.EmailField(blank=True, null=True, unique=True)
     phone_no = models.CharField(unique=True, max_length=12)
-    address = models.TextField(null=True)
-
-    #Cource interested
     course_interested = models.CharField(max_length=100)
-    mode = models.CharField(max_length=100, choices=MODE, default='offline')
-
-    # Source and Status of Leads
-    source = models.CharField(max_length=100, choices=SOURCE_CHOICES)
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='new')
-
+    lead_source = models.CharField(max_length=100, choices=SOURCE_CHOICES)
     assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)    
-
+    initial_notes = models.TextField(null=True, blank=True)
+    enquiry_date = models.DateField()
+    next_followup_date = models.DateField()
+    initial_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    
     def __str__(self):
-        return self.full_name
-
-# Call-Log Model
+        return self.lead_name
+    
+# Call-Log model
 class CallLog(models.Model):
-
-    CALL_TYPE_CHOICES = [
-        ('incomming', 'Incomming'),
-        ('outgoing', 'Outgoing')
+    LEAD_STATUS_CHOICES = [
+        ('Interested', 'Interested'),
+        ('Not Reachable', 'Not Reachable'),
+        ('Demo Scheduled', 'Demo Scheduled'),
+        ('Not Interested', 'Not Interested'),
     ]
+    def __str__(self):
+        return self.lead_name
 
-    lead = models.ForeignKey(Lead, on_delete=models.CASCADE, related_name='call_logs')
+    lead_name = models.CharField(max_length=100,null=True, blank= True)
 
-    call_type = models.CharField(max_length=20, choices=CALL_TYPE_CHOICES)
-    call_time = models.TimeField(auto_now_add=True)
-    duration = models.IntegerField(help_text="Duration in seconds")
+
+    call_date = models.DateField(max_length=100,null=True, blank= True)
+    call_time = models.TimeField(max_length=100,null=True, blank= True)
+
+    duration = models.IntegerField(null=True, blank= True)  
+
+    call_outcome = models.CharField(max_length=50, choices=LEAD_STATUS_CHOICES)
 
     notes = models.TextField()
 
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    next_followup_date = models.DateField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.lead.full_name} - {self.call_type}"
-    
-# Follow-Up
-class Follow_up(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('done', 'Done'),
-        ('missed', 'Missed'),
-    ]
-
-    lead = models.ForeignKey(Lead, on_delete=models.CASCADE, related_name='followups')
-    follow_up_date = models.DateTimeField()
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='pending')
-    remarks = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return f"{self.lead.full_name} - {self.follow_up_date}"
