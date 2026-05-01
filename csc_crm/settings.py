085858977b5bba6a9c157b08aa5150f0ea1446a0
Sathish Vishnu <sathishvishnuarishkumar@gmail.com>
2026-05-01 11:34:07 +0530

    Ready for Render deployment

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_r7-ke=d^fsmsl=l1*(0#^2-*nngv*$_jg_)x(r0a*gh^o%dzr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
